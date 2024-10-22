ecc8103e017ef9eaa4b7200f46307ba3c15ff1cf
Itztiva <tiva@Itztiva.com>
2024-10-22 17:29:33 -0400

    I dont like warnings

--- Xtract/Providers/FileProvider.cs
+++ Xtract/Providers/FileProvider.cs
@@ -11,60 +11,63 @@ using CUE4Parse.UE4.Versions;
 
 namespace Xtract.Providers
 {
-  public class FileProvider
-  {
-    public DefaultFileProvider _fileProvider { get; private set; }
-    private readonly string _path;
-    private readonly EGame _ueVersion;
-
-    public FileProvider(string path, EGame ueVersion)
+    public class FileProvider
     {
-      _path = path;
-      _ueVersion = ueVersion;
-    }
+        public DefaultFileProvider _fileProvider { get; private set; } = null!;
+        private readonly string _path;
+        private readonly EGame _ueVersion;
 
-    /// <summary>
-    /// Initializes the FileProvider with the specified game directory and unreal engine version.
-    /// </summary>
-    public async Task Initialize()
-    {
-      try
-      {
-        _fileProvider = new DefaultFileProvider(_path, SearchOption.AllDirectories, true, new VersionContainer(_ueVersion));
-        _fileProvider.Initialize();
-        _fileProvider.LoadLocalization();
-
-        // TODO: Mappings & aes keys
-
-        var keysCount = _fileProvider.Keys.Count;
-        if (keysCount == 0)
+        public FileProvider(string path, EGame ueVersion)
         {
-          throw new Exception("No keys found.");
+            _path = path;
+            _ueVersion = ueVersion;
         }
 
-        Console.WriteLine($"FileProvider initialized successfully with {keysCount} {(keysCount == 1 ? "key" : "keys")}.");
-
-                Parallel.ForEach(_fileProvider.MountedVfs, vfs =>
+        /// <summary>
+        /// Initializes the FileProvider with the specified game directory and unreal engine version.
+        /// </summary>
+        public async Task Initialize()
+        {
+            try
+            {
+                await Task.Run(() =>
                 {
-                    Console.WriteLine($"Successfully mounted {vfs.Name} at {vfs.MountPoint}.");
+                    _fileProvider = new DefaultFileProvider(_path, SearchOption.AllDirectories, true, new VersionContainer(_ueVersion));
+                    _fileProvider.Initialize();
+                    _fileProvider.LoadLocalization();
+
+                    // TODO: Mappings & aes keys
+
+                    var keysCount = _fileProvider.Keys.Count;
+                    if (keysCount == 0)
+                    {
+                        throw new Exception("No keys found.");
+                    }
+
+                    Console.WriteLine($"FileProvider initialized successfully with {keysCount} {(keysCount == 1 ? "key" : "keys")}.");
+
+                    Parallel.ForEach(_fileProvider.MountedVfs, vfs =>
+                    {
+                        Console.WriteLine($"Successfully mounted {vfs.Name} at {vfs.MountPoint}.");
+                    });
                 });
-      }
-      catch (FileNotFoundException ex) 
-      {
-        throw new FileNotFoundException("Game directory not found.", ex);
-      }
-      catch (UnauthorizedAccessException ex)
-      {
-        throw new UnauthorizedAccessException("Game directory not accessible.", ex);
-      }
-      catch (DirectoryNotFoundException ex)
-      {
-        throw new DirectoryNotFoundException("Game directory not found.", ex);
-      }
-      catch (Exception ex)
-      {
-        throw new Exception("Failed to initialize FileProvider.", ex);
-      }
+            }
+            catch (FileNotFoundException ex)
+            {
+                throw new FileNotFoundException("Game directory not found.", ex);
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                throw new UnauthorizedAccessException("Game directory not accessible.", ex);
+            }
+            catch (DirectoryNotFoundException ex)
+            {
+                throw new DirectoryNotFoundException("Game directory not found.", ex);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Failed to initialize FileProvider.", ex);
+            }
+        }
     }
-  }
 }
